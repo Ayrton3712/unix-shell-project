6455d6107819a116acd1457ef0086850af7cdc4d
Ayrton3712 <mugittty@gmail.com>
2026-03-28 19:35:39 -0300

    Prompt Display: Basic continuous shell loop finished

--- src/main.c
+++ src/main.c
@@ -1,12 +1,28 @@
 #include <stdio.h>
+#include <string.h>
 #include <unistd.h>
 
 int main(){
     char cwd[1024];
     getcwd(cwd, sizeof(cwd));
-    
-    // Basic shell prompt
-    
-    printf("mysh:%s> \n", cwd);
+
+    char input[100];
+    // Continuous shell loop
+    while(1){
+        printf("mysh:%s> ", cwd);               // Shell prompt
+        
+        fgets(input, sizeof(input), stdin);     // Reading user input with fgets
+
+        // Replacing trailing \n with \0
+        size_t len = strlen(input);
+        if (input[len - 1] == '\n') input[len - 1] = '\0';
+
+        // Edge case: Empty input
+        // TODO
+
+        // Exit condition
+        if (strcmp(input, "exit") == 0) break;
+    }
+
     return 0;
 }
