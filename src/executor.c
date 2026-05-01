e5271cf6093d1d4c7ace750439d98fa46c3440c3
Ayrton3712 <mugittty@gmail.com>
2026-05-01 19:07:00 -0300

    Basic executor implemented

--- src/executor.c
+++ src/executor.c
@@ -1,4 +1,18 @@
 #include <unistd.h>
+#include <sys/wait.h>
 
 #include "../include/executor.h"
 
+int executor(Command cmd){
+    pid_t pid = fork();
+
+    if (pid == 0){
+        execvp(cmd.name, cmd.args);
+    }
+    else if (pid == -1){
+        printf("Child process could not be created.\n");
+    }
+    else{
+        waitpid(pid, NULL, 0);
+    }
+}
