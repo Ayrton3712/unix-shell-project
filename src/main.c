57838f04c2025cc5bec16b7e13c35b19d9e7a1d6
Ayrton3712 <mugittty@gmail.com>
2026-05-02 16:15:31 -0300

    Built-in commands: Fix typo in help print message

--- src/main.c
+++ src/main.c
@@ -56,7 +56,7 @@ int main(){
         if (strcmp(cmd.name, "help") == 0){
             printf("cd dir  \t - Change directory to dir\n
                     pwd     \t - Print working directory\n
-                    exit    \t - Exit the shell
+                    exit    \t - Exit the shell\n
                     Note: Other commands are passed to the OS via execvp (ls, cat, etc.)\n");
         }
         // exit
