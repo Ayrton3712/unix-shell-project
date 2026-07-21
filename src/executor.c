3561596ac87822b58f0cf5cb2950a7442404580c
Ayrton3712 <mugittty@gmail.com>
2026-07-21 17:50:14 -0300

    feat(executor): implement output redirection

--- src/executor.c
+++ src/executor.c
@@ -2,7 +2,8 @@
 #include <stdlib.h>
 
 #include <unistd.h>
-#include <sys/wait.h>
+#include <sys/wait.h>   // for waitpid()
+#include <fcntl.h>      // for open()
 
 #include "../include/executor.h"
 
@@ -10,6 +11,13 @@ void executor(Command cmd){
     pid_t pid = fork();
 
     if (pid == 0){ // Child path
+        // Output redirection
+        if (cmd.output_file != NULL){
+            int file_fd = open(cmd.output_file, O_WRONLY | O_CREAT | O_TRUNC);  // Open the file and store its fd
+            dup2(file_fd, STDOUT_FILENO);   // Redirect fd 1 to file_fd
+            close(file_fd); // Close file_fd
+        }
+
         execvp(cmd.name, cmd.args);
         perror(cmd.name);
         exit(EXIT_FAILURE);
